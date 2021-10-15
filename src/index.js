731c851e517786331c9ceaa0eec60ebfdcf51c3a
Vanessa Schade <82608228+travellingcolognian@users.noreply.github.com>
2021-10-15 18:17:53 +0200

    Changed the styling of the Vanilla Weather App

--- src/index.js
+++ src/index.js
@@ -86,7 +86,7 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         `
-      <div class="col-2">
+      <div class="col-2 mx-sm">
       <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
       <img
         src="http://openweathermap.org/img/wn/${
